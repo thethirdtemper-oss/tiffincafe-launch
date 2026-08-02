e13c48312dcd42eac15a681d92889fa84d05d9ca
thethirdtemper-oss <thethirdtemper@gmail.com>
2026-08-02 20:51:42 +0530

    Update countdown.js

---
+++ countdown.js
@@ -0,0 +1,45 @@
+// Launch Date
+const launchDate = new Date("August 15, 2026 11:30:00 GMT+0530").getTime();
+
+function updateCountdown() {
+
+    const now = new Date().getTime();
+
+    const distance = launchDate - now;
+
+    if (distance <= 0) {
+
+        document.getElementById("countdown").innerHTML =
+        "🎉 WE ARE LIVE!";
+
+        return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("countdown").innerHTML =
+        days + " Days " +
+        hours + " Hours " +
+        minutes + " Minutes " +
+        seconds + " Seconds";
+
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, 1000);
